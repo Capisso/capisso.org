ff1e40188d1c5f2102c5ee95af2dc729f32afd15
clone1018 <luke@axxim.net>
2013-06-24 13:30:11 -0500

    One more D:

--- app/handlers/UserHandler.php
+++ app/handlers/UserHandler.php
@@ -6,7 +6,7 @@ class UserHandler
     public function onRegister($data, $activationCode)
     {
         Mail::send('emails.auth.register', array('user' => $data, 'activationCode' => $activationCode), function ($m) use ($data) {
-            $m->to($data->email)->subject('Welcome to Storage!');
+            $m->to($data->email)->subject('Welcome to Capisso!');
         });
     }
 
