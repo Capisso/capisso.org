460f30f53deadffc0c13405bda07ad9f930e0d95
clone1018 <luke@axxim.net>
2013-06-24 13:29:22 -0500

    Naming

--- app/views/account/thanks.blade.php
+++ app/views/account/thanks.blade.php
@@ -5,7 +5,7 @@ Thanks For Registering!
 @stop
 
 @section('content')
-<h3>Welcome to Storage!</h3>
+<h3>Welcome to Capisso!</h3>
 <p>First off, check your email for the verification link we sent you, if you don't receive this just let us know at
     <a href="mailto:help@capisso.org">help@capisso.org</a></p>
 @stop
