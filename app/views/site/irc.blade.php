c03b91a8ccad1303a99c897730ee7834d36eb274
Kamal Nasser <kamal@kamalnasser.net>
2013-06-25 12:35:56 +0300

    Set the IRC webchat widget's width to 100%

--- app/views/site/irc.blade.php
+++ app/views/site/irc.blade.php
@@ -3,5 +3,5 @@
 @section('content')
 <p>Come chat on Freenode at #capisso</p>
 
-<iframe src="http://webchat.freenode.net?channels=capisso&uio=d4" width="647" height="400"></iframe>
+<iframe src="http://webchat.freenode.net?channels=capisso&uio=d4" width="100%" height="400"></iframe>
 @stop
